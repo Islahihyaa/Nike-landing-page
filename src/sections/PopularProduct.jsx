f37e219df84c85c41457159e2aaf7d90bb689c5a
Islahihyaa <islah.ihya@gmail.com>
2024-02-05 20:42:40 +0700

    Create Popular Product page

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -11,8 +11,8 @@ const PopularProduct = () => {
           <h1 className='text-4xl font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h1>
           <p className='text-lg text-slate-600 max-w-lg mb-6'>Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value</p>
         </div>
-        <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          <div className="flex flex-col w-full">
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          <div className="flex flex-col justify-start">
               <img 
               src={bigShoe2}
               alt="bigShoe2"
@@ -31,6 +31,16 @@ const PopularProduct = () => {
               height={150}
               className="border p-4 bg-primary rounded-lg" />
             <h2 className="font-semibold text-lg mt-6 font-montserrat">Nike Air Jordan-01</h2>
+            <p className="font-bold text-2xl text-coral-red mt-1">$200.20</p>
+          </div>
+          <div className="flex flex-col justify-start">
+              <img 
+              src={bigShoe2}
+              alt="bigShoe2"
+              width={200}
+              height={150}
+              className="border p-4 bg-primary rounded-lg" />
+            <h2 className="font-semibold text-lg  mt-6 font-montserrat">Nike Air Jordan-01</h2>
             <p className="font-bold text-2xl text-coral-red mt-1">$200.20</p>
           </div>
           <div className="flex flex-col justify-start">
